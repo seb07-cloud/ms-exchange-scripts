d6f761b42033c65b74bbd0ea750f1be03be2a282
seb07 <wild.sebastian@outlook.de>
2021-10-17 09:32:56 +0200

    Update set-immutable.ps1

--- set-immutable.ps1
+++ set-immutable.ps1
@@ -1,6 +1,6 @@
 foreach ($ADUser in (get-adgroupmember -identity G_O365))
 	{
-		$365User = $aduser.samaccountname + "@biberkopf.at"
+		$365User = $aduser.samaccountname + "@domain.com"
 		$guid =(Get-ADUser $ADUser).Objectguid
 		$immutableID=[system.convert]::ToBase64String($guid.tobytearray())
         #$365Acc = Get-MsolUser -UserPrincipalName $O365User
